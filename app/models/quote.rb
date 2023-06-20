fe696c0663bd98a63de12455880fbbcd17050485
Rafael Moreira <rafamsilva@gmail.com>
2023-06-20 13:11:40 +1000

    added broadcast for EDIT and destroy quotes.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 
   # after_create_commit :broadcast_quote
 
