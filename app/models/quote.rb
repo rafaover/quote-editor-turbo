b1201d1bbf79667dba3845c0257cec09136bd916
Rafael Moreira <rafamsilva@gmail.com>
2023-06-20 12:25:56 +1000

    broadcast update - added a broadcast on quote model, for notifications. - appended the model broadcast to index

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,11 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
+  after_create_commit :broadcast_quote
+
+  def broadcast_quote
+    broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes"
+  end
 end
