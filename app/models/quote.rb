329053e3e55f80018b11a4758538ae730d40607d
Rafael Moreira <rafamsilva@gmail.com>
2023-06-26 12:14:15 +1000

    updated models with associations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   validates :name, presence: true
 
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
@@ -7,8 +9,8 @@ class Quote < ApplicationRecord
   # all 3 above, is the same of the one below
   broadcasts_to ->(_quote) { "quotes" }, inserts_by: :prepend
 
+  # Another solution
   # after_create_commit :broadcast_quote
-
   # def broadcast_quote
   #   broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes"
   # end
