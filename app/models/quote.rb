17b8439f4c40d72d41a796ee6bbc19f79b56e09a
Rafael Moreira <rafamsilva@gmail.com>
2023-06-26 13:44:30 +1000

    SECURITY UPDATE FOR TURBO_STREAM: - On the model. The stream now will use an array for key ident. - The update must reflect on the prepend inside quotes#index

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,7 +7,23 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # all 3 above, is the same of the one below
-  broadcasts_to ->(_quote) { "quotes" }, inserts_by: :prepend
+
+  # We have to change the stream name where the quotes' HTML will be broadcasted.
+
+  # Under the hood, the signed stream name is generated from the array returned
+  # by the lambda that is the first argument of the broadcasts_to method. The
+  # rules for secure broadcastings are the following:
+
+  # Users who share broadcastings should have the lambda return an array
+  # with the same values.
+
+  # Users who shouldn't share broadcastings should have the lambda return an
+  # array with different values.
+
+  # Check quotes#index where the turbo_stream_from is defined. It returns
+  # similar array.
+
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   # Another solution
   # after_create_commit :broadcast_quote
