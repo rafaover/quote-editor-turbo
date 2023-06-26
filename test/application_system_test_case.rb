6cf652c29ba0ce59642124b80096350a97a4e720
Rafael Moreira <rafamsilva@gmail.com>
2023-06-26 13:10:22 +1000

    added WARDEN to application system Test class. - check comment for details.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+# To sign in users in system tests, we will rely on helpers from the Warden gem
+# to avoid coding them ourselves. Devise is built on top of Warden and the
+# Warden::Test::Helpers module contains helpers that will help us login users
+# in our tests thanks to the login_as method.
+
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Warden::Test::Helpers
+
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
