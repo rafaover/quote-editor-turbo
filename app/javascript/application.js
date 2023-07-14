278df724bc3c4053d0731a8a8cd7a25aefd80fad
Rafael Moreira <rafamsilva@gmail.com>
2023-07-15 09:32:07 +1000

    disabled turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,6 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.session.drive = false;
