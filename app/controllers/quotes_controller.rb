b8b9c536f84807e2be3f90260d9f7bf014b0a2c7
Rafael Moreira <rafamsilva@gmail.com>
2023-07-15 09:56:27 +1000

    Updated qyote controller. - Added flash.now on actions to be used with turbostream

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -19,7 +19,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -39,7 +39,7 @@ class QuotesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully deleted." }
     end
   end
 
