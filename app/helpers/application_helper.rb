85b02202b40fae9862f8185bc6b1797047b1eee8
Rafael Moreira <rafamsilva@gmail.com>
2023-07-15 10:05:15 +1000

    Created helper to render flash msg. DRY method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Render flash messages with xxx.turbo_stream.erb files inside views
+  def render_turbo_flashmsg
+    turbo_stream.prepend "flash", partial: "layouts/flash"
+  end
 end
